48835b1db6461736df3bc39677cd7877edb5ee1f
SalikWarsi <salikwarsi@gmail.com>
2018-05-07 18:10:08 -0700

    Fixing the issue to test if file exists

--- homework3.py
+++ homework3.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import os as os
 query1 = "select video_id, category_id, \"us\" as language from USvideos "
 query2 = "UNION select video_id, category_id, \"ca\" as language from CAvideos "
 query3 = "UNION select video_id, category_id, \"gb\" as language from GBvideos "
@@ -7,6 +8,8 @@ query4 = "UNION select video_id, category_id, \"fr\" as language from FRvideos "
 query5 = "UNION select video_id, category_id, \"de\" as language from DEvideos;"
 query = query1 + query2 + query3 + query4 + query5
 def create_dataframe(dbpath):
+    if not os.path.isfile(dbpath):
+        raise ValueError("SQLite db not found")
     try:
         conn = sqlite3.connect(dbpath)
         df = pd.read_sql_query(query, conn)
